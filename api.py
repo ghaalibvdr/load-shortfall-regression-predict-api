5fb23d7a59fab597751332be88eb703b046959a2
Ghaalib <ghaalibvdr@gmail.com>
2023-05-20 15:20:21 +0200

    hello world

--- api.py
+++ api.py
@@ -54,6 +54,7 @@ def model_prediction():
     # prediction.
     output = make_prediction(data, static_model)
     print(output)
+    print("Hello World")
     # We finally package this prediction as a JSON object to deliver a valid
     # response with our API.
     return jsonify(output)
